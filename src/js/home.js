bfde0adccffebf30f6d85501abfbd53a5cee756f
Hellwyn <elvinkauffmann@gmail.com>
2023-12-10 17:26:32 +0100

    slider fini

--- src/js/home.js
+++ src/js/home.js
@@ -4,16 +4,19 @@ function init () {
 // Changement des films de l'accueil
 
 function slider () {
+    const parent = document.querySelector("ul#slider");
+    
     setInterval(function () {
         let elements = document.getElementsByClassName("slide-item");
 
-        let newLi = '<li class="slide-item" id="previous-slide-item" data-id="1"><img src="../src/img/film0.jpg" alt=""></li>';
-        let parent = document.querySelector("ul#slider");
-
+        let idNewImg = 0;
         for (let item of elements) {
             let id = parseInt(item.getAttribute("data-id"))
             item.setAttribute("data-id", id+1)
+            idNewImg = parseInt(item.getAttribute("data-img"));
         }
+
+        let newLi = '<li class="slide-item" id="previous-slide-item" data-id="1" data-img="'+idNewImg+'"><img src="../src/img/films/film'+idNewImg+'.jpg" alt=""></li>';
 
         parent.removeChild(parent.lastElementChild);
         parent.insertAdjacentHTML('afterbegin', newLi)
