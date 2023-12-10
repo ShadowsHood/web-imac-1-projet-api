acec2b8e2dbd916201c1cc82540981ade0dec32c
Hellwyn <elvinkauffmann@gmail.com>
2023-12-10 18:14:10 +0100

    style home et general

--- src/js/home.js
+++ src/js/home.js
@@ -25,20 +25,20 @@ function slider () {
 
 // Affichage des 3 top film
 
-async function afficherInBox(name, boxNumber){
-    const responseOMDB = await fetch("http://www.omdbapi.com/?t="+name+"&apikey=2d2c5592");
-    const film = await responseOMDB.json();
-    console.log(film);
-    document.querySelector("#boite"+boxNumber).innerHTML = `
-        <img src="`+film.Poster+`" alt="Poster du film`+film.Title+`">
-        <p>`+film.Title+`</p>
-        <p>`+film.Director+`</p>
-        <p>`+film.Released+`</p>
-    `;
-};
+// async function afficherInBox(name, boxNumber){
+//     const responseOMDB = await fetch("http://www.omdbapi.com/?t="+name+"&apikey=2d2c5592");
+//     const film = await responseOMDB.json();
+//     console.log(film);
+//     document.querySelector("#boite"+boxNumber).innerHTML = `
+//         <img src="`+film.Poster+`" alt="Poster du film`+film.Title+`">
+//         <p>`+film.Title+`</p>
+//         <p>`+film.Director+`</p>
+//         <p>`+film.Released+`</p>
+//     `;
+// };
 
-afficherInBox("Avatar", "1");
-afficherInBox("Wall E", "2");
-afficherInBox("Harry Potter", "3");
+// afficherInBox("Avatar", "1");
+// afficherInBox("Wall E", "2");
+// afficherInBox("Harry Potter", "3");
 
 
