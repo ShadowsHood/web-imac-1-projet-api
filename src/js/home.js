3cf05c952902d9527704e069e92126fdc8d55f92
Hellwyn <elvinkauffmann@gmail.com>
2023-12-11 19:28:30 +0100

    tout bien fait parfait tout ensemble mais pas total fini

--- src/js/home.js
+++ src/js/home.js
@@ -10,13 +10,15 @@ function slider () {
         let elements = document.getElementsByClassName("slide-item");
 
         let idNewImg = 0;
+        let altNewImg = 0;
         for (let item of elements) {
             let id = parseInt(item.getAttribute("data-id"))
             item.setAttribute("data-id", id+1)
             idNewImg = parseInt(item.getAttribute("data-img"));
+            altNewImg = item.querySelector("img").getAttribute("alt");
         }
 
-        let newLi = '<li class="slide-item affiche" id="previous-slide-item" data-id="1" data-img="'+idNewImg+'"><img src="../src/img/films/film'+idNewImg+'.jpg" alt=""></li>';
+        let newLi = '<li class="slide-item affiche" id="previous-slide-item" data-id="1" data-img="'+idNewImg+'"><img src="../src/img/films/film'+idNewImg+'.jpg" alt="'+altNewImg+'"></li>';
 
         parent.removeChild(parent.lastElementChild);
         parent.insertAdjacentHTML('afterbegin', newLi)
